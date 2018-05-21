01535a4130db1baa5fa5dc3db462cca0f4f52e9b
alycianielsen <39496926+alycianielsen@users.noreply.github.com>
2018-05-21 15:26:48 -0400

    updated

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ tl.from(starsone, 1.5, {alpha:0});
 
 tl.from(starstwo, 2, {alpha:0});
 
-tl.from(earth, 2.5, {alpha:0});
+// tl.from(earth, 2.5, {alpha:0});
 
-tl.from(planet, 2.7, {alpha:0});
+// tl.from(planet, 2.7, {alpha:0});
 
+
+var tl = new TimelineLite();
+tl.from("#earth", 1.5, {alpha:0}, "+=1")
+  .from("#planet", 1, {alpha:0}, "+=1")
+
+var tl = new TimelineLite();
+tl.from("spaceship", 1.5, {scaleY:750})
